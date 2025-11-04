7cd18b717e06fe2fffb3968178d234a448462f85
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-11-04 13:48:00 +0500

    [04] random matrix generation

---
+++ 04_random_vector/src/utils.cuh
@@ -0,0 +1,38 @@
+#ifndef UTILS_CUH_
+#define UTILS_CUH_
+
+#include <iostream>
+
+// Utility function to print matrix
+template <typename T>
+void print_matrix(T *h_matrix, int rows, int cols, int max_display = 10) {
+  int display_rows = (rows < max_display) ? rows : max_display;
+  int display_cols = (cols < max_display) ? cols : max_display;
+
+  std::cout << "Matrix (" << rows << "x" << cols;
+  if (rows > max_display || cols > max_display) {
+    std::cout << ", showing " << display_rows << "x" << display_cols;
+  }
+  std::cout << "):" << std::endl;
+
+  for (int i = 0; i < display_rows; i++) {
+    for (int j = 0; j < display_cols; j++) {
+      std::cout << h_matrix[i * cols + j] << "\t";
+    }
+    if (cols > max_display) {
+      std::cout << "...";
+    }
+    std::cout << std::endl;
+  }
+  if (rows > max_display) {
+    for (int j = 0; j < display_cols; j++) {
+      std::cout << ".\t";
+    }
+    if (cols > max_display) {
+      std::cout << "...";
+    }
+    std::cout << std::endl;
+  }
+}
+
+#endif // UTILS_CUH_
